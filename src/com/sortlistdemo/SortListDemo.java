238ef42931b19868bda87f5dc047c0ac9c38c9d9
Surobhi <surobhi1395@gmail.com>
2021-09-22 19:08:16 +0530

    Commiting Files

---
+++ src/com/sortlistdemo/SortListDemo.java
@@ -0,0 +1,13 @@
+package com.sortlistdemo;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SortListDemo {
+
+    List<Integer> list = new ArrayList<>();
+    
+    // traditional way of sorting a list
+
+}
---
+++ src/com/streamapi/demo/EmployeeService.java
@@ -0,0 +1,33 @@
+package com.streamapi.demo;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+// writing logic to check whether the employee is eligible for the tax or not
+public class EmployeeService {
+
+    public static List<Employee> checkTaxUsers(String input){
+        // getting list of all employees first
+        // using filter to get employees having salary > 50,000
+        /* if(input.equalsIgnoreCase("tax")) {
+            return EmployeeDao.getAllEmp().stream()
+                    .filter(emp -> emp.getSalary() > 50000).collect(Collectors.toList());
+        }
+       else
+           return EmployeeDao.getAllEmp().stream()
+                   .filter(emp-> emp.getSalary()<50000).collect(Collectors.toList());
+                   */
+
+        // with ternary operator
+        return (input.equalsIgnoreCase("tax"))
+                ? EmployeeDao.getAllEmp().stream().filter(employee->employee.getSalary()>50000)
+                  .collect(Collectors.toList())
+                : EmployeeDao.getAllEmp().stream().filter(employee->employee.getSalary()<50000)
+                   .collect(Collectors.toList());
+
+    }
+
+    public static void main(String[] args){
+        System.out.println(checkTaxUsers("tax"));
+    }
+}
